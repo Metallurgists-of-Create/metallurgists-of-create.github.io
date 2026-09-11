bca4d939d06d0afaba8b9894e9e4e968ad365715
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2026-09-11 10:13:35 -0400

    TFMG Sidebars!!!

--- .vitepress/sidebars/tfmg.ts
+++ .vitepress/sidebars/tfmg.ts
@@ -25,7 +25,32 @@ export default {
                     collapsed: true,
                     items: [
                         { text: 'Datapack', link: '/tfmg/pack-developers/datapack' },
-                        { text: 'KubeJS', link: '/tfmg/pack-developers/kubejs' }
+                        {
+                            text: 'KubeJS',
+                            link: '/tfmg/pack-developers/kubejs',
+                            collapsed: true,
+                            items: [
+                                {
+                                    text: 'Recipes',
+                                    link: '/tfmg/pack-developers/kubejs#recipes',
+                                    collapsed: true,
+                                    items: [
+                                        { text: 'Casting', link: '/tfmg/pack-developers/kubejs#casting' },
+                                        { text: 'Coking', link: '/tfmg/pack-developers/kubejs#coking' },
+                                        { text: 'Distillation', link: '/tfmg/pack-developers/kubejs#distillation' },
+                                        { text: 'Industrial Blasting', link: '/tfmg/pack-developers/kubejs#industrial-blasting' },
+                                        { text: 'Polarizing', link: '/tfmg/pack-developers/kubejs#polarizing' },
+                                        { text: 'Winding', link: '/tfmg/pack-developers/kubejs#winding' },
+                                        { text: 'Hot Blasting (aka Air Blasting)', link: '/tfmg/pack-developers/kubejs#hot-blasting-aka-air-blasting' },
+                                        { text: 'Chemical Vat', link: '/tfmg/pack-developers/kubejs#chemical-vat' }
+                                    ]
+                                },
+                                {
+                                    text: 'Custom Cable Types and Electrodes',
+                                    link: '/tfmg/pack-developers/kubejs#custom-cable-types-and-electrodes'
+                                }
+                            ]
+                        }
                     ]
                 }
             ]
